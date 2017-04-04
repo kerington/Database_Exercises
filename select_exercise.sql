ef9b6368d5fb5c3b1ee480b62dcdd971f40d2959
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-04 12:30:23 -0500

    Updating 	- Select Exercise
    Adding
    	- Update Exercise

--- select_exercise.sql
+++ select_exercise.sql
@@ -1,17 +1,22 @@
 USE codeup_test_db;
 
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
-SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
-SELECT genre FROM albums WHERE name = 'Nevermind';
-SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
-SELECT name FROM albums WHERE sales < 20000000;
-SELECT name FROM albums WHERE genre = '%rock%'; 
-
-
-
 -- The name of all albums by Pink Floyd.
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+
 -- The genre for Nevermind
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
 -- Which albums were released in the 1990s
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999\G
+
 -- Which albums had less than 20 million certified sales
+SELECT name FROM albums WHERE sales < 20000000;
+
 -- All the albums in the rock genre. Is this all the rock albums in the table?
+SELECT name FROM albums WHERE genre LIKE '%rock%';
+
+-- All the artists name in alphabetical order
+SELECT artist FROM albums ORDER BY artist;
