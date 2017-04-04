dbf31d702470bde5a22d8a4bf5ea53254752de98
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-04 11:26:58 -0500

    Adding the following exercises completed in class 	- Albums Migration 	- Albums Seeder 	- Select Exercise

---
+++ select_exercise.sql
@@ -0,0 +1,17 @@
+USE codeup_test_db;
+
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT name FROM albums WHERE sales < 20000000;
+SELECT name FROM albums WHERE genre = 'rock'; 
+
+
+
+-- The name of all albums by Pink Floyd.
+-- The year Sgt. Pepper's Lonely Hearts Club Band was released
+-- The genre for Nevermind
+-- Which albums were released in the 1990s
+-- Which albums had less than 20 million certified sales
+-- All the albums in the rock genre. Is this all the rock albums in the table?
