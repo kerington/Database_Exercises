2449eef5ae37015c82c65feb2fdf963b10fb9b36
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-05 12:00:25 -0500

    COMPLETED the following task for Order By Exercises
    -- Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -16,13 +16,22 @@
 -- ORDER BY first_name;
 
 -- DONE -- Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+-- SELECT * FROM employees
+-- WHERE (first_name
+-- = 'Irena'
+-- OR first_name = 'Vidya'
+-- OR first_name = 'Maya')
+-- AND gender = 'M'
+-- ORDER BY first_name, last_name;
+
+-- Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
 SELECT * FROM employees
 WHERE (first_name
 = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya')
 AND gender = 'M'
-ORDER BY first_name, last_name;
+ORDER BY last_name, first_name;
 
 -- DONE -- Employees whose last name starts with 'E' — 7,330 rows.
 -- SELECT DISTINCT *
@@ -91,7 +100,7 @@ NOT LIKE '%qu%';
 
 
 
--- Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+
 -- Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
 -- Now reverse the sort order for both queries.
 -- Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
