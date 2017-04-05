d12a41be7a9d3727379951047648650b79bfafc6
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-05 11:20:52 -0500

    COMPLETED the following for where_exercises
    -- Find all employees whose last name starts or ends with 'E' — 30,723 rows.

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,9 +2,9 @@
 -- SELECT * FROM employees
 -- WHERE first_name
 -- IN ('Irena', 'Vidya', 'Maya');
+
 -- DONE -- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
 -- DONE -- Now add a condition to find everybody with those names who is also male — 441 rows.
-
 
 SELECT * FROM employees
 WHERE (first_name
@@ -15,12 +15,18 @@ AND gender = 'M';
 
 
 -- DONE -- Employees whose last name starts with 'E' — 7,330 rows.
+-- SELECT DISTINCT *
+-- FROM employees
+-- WHERE last_name
+-- LIKE 'e%';
 
 -- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
 SELECT DISTINCT *
 FROM employees
 WHERE last_name
-LIKE 'e%';
+LIKE 'e%'
+OR last_name
+LIKE '%e';
 
 -- DONE -- Employees hired in the 90s — 135,214 rows.
 SELECT * FROM employees
