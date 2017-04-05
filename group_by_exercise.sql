2f12607280561cdf713627fb40694cdca67bfcd4
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-05 13:53:00 -0500

    COMPLETED the following tasks for Group By Exercise
    -- DONE -- Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
    -- DONE -- Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -5,7 +5,7 @@ FROM titles
 ORDER BY title;
 
 
--- Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY.
+-- DONE -- Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY.
 SELECT DISTINCT last_name
 FROM employees
 WHERE last_name
@@ -13,3 +13,25 @@ LIKE 'e%'
 AND last_name
 LIKE '%e'
 GROUP BY last_name;
+
+-- Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+SELECT DISTINCT first_name, last_name
+FROM employees
+WHERE last_name
+LIKE 'e%'
+AND last_name
+LIKE '%e';
+
+-- Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY. 
+
+SELECT DISTINCT *
+FROM employees
+WHERE last_name
+LIKE '%q%'
+AND last_name
+NOT LIKE '%qu%'
+GROUP BY last_name;
+
+
+
+
