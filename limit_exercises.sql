4ec615d5a624f8b23b409f77ce8dc5401c0415d2
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-05 14:16:11 -0500

    COMPLETED the following tasks for Limit Exercises
    -- DONE -- Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees. Their names should be:
    -- DONE -- Try to think of your results as batches, or sets. The first five results are your first batch. The five after that would be your second batch, etc. Update the query to find the tenth batch of results. The employee names should be:

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,4 +1,16 @@
-SELECT DISTINCT last_name
+-- DONE -- List the first 10 distinct last name sorted in descending order. Your result should look like this:SELECT DISTINCT last_name
 FROM employees
-ORDER BY DESC
+ORDER BY last_name DESC
 LIMIT 10;
+
+
+-- Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees. Their names should be:
+SELECT * FROM employees
+WHERE hire_date
+LIKE '199%'
+AND birth_date
+LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+-- Try to think of your results as batches, or sets. The first five results are your first batch. The five after that would be your second batch, etc. Update the query to find the tenth batch of results. The employee names should be:
