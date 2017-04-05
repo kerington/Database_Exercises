5c24d30880e5f3a6b7554aaf2a456cb39a52f8f8
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-05 14:35:00 -0500

    COMPLETED the following task for Functions Exercises
    -- For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -13,8 +13,9 @@ WHERE last_name LIKE 'e%'
 AND last_name LIKE '%e';
 
 
-
-SELECT * FROM employees
+-- For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
+SELECT DATEDIFF(CURDATE(), hire_date)
+FROM employees
 WHERE hire_date
 LIKE '199%'
 AND birth_date
